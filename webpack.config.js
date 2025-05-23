99d66a323ddeb7a4d76ef6de77e0defb5727a478
root <alex.leibowitz@zoro.com>
2025-05-23 22:41:46 +0000

    fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     mode: "development",
@@ -34,6 +35,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./public/index.html"
+        }),
+        new CopyPlugin({
+            patterns: [
+                {"from": "./public", "to": ".", "globOptions": {ignore: ["**/*.html"]}},
+            ]
         })
     ]
 }
