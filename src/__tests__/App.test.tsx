5e18df2c93db051809070dba2bde39c7b68df5fa
root <alex.leibowitz@zoro.com>
2025-05-23 22:54:05 +0000

    as an exercise, refine test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,7 +4,10 @@ import { App } from "@/App";
 describe("App component", () => {
   it("renders the main heading", () => {
     render(<App />);
-    const heading = screen.getByText(/hello, interlinear!/i);
+    const heading = screen.getByRole('heading');
+    const image = screen.getByRole('img')
     expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Hello, interlinear!')
+    expect(image).toBeInTheDocument();
   });
 });
