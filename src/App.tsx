89355d40f71d7b870296a0dc8bbe023a21f551d2
root <alex.leibowitz@zoro.com>
2025-05-28 00:59:10 +0000

    add more passages; enable gloss on clicking a word; add linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import React from "react";
 
-import { Passage } from "@/types/passage";
+import { Gloss, Passage } from "@/types/passage";
 import { FileClient } from "@/clients/file"
 import TokenBlock from "@/TokenBlock";
 
@@ -8,27 +9,39 @@ const passageOptions = [
     { key: 'oku-no-hosomichi', name: 'Oku No Hosomichi'},
     { key: 'ovid', name: 'Ovid\'s Metamorphoses'},
     { key: 'quran', name: 'The Quran'},
-    { key: 'lun-yu', name: 'Analects of Confucius'}
+    { key: 'lun-yu', name: 'Analects of Confucius'},
+    { key: 'notes-underground', name: 'Notes From Underground'}
 ]
 
 function App() {
     const [selectedKey, setSelectedKey] = useState("oku-no-hosomichi");
     const [passage, setPassage] = useState<Passage | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [selectedToken, setSelectedToken] = useState<number>(-1);
+    const [selectedGlosses, setSelectedGlosses] = useState<Gloss[]>([]);
 
     useEffect(() => {
-        const load = async () => {
+        (async function () {
             try {
                 const client = FileClient.getClient();
                 const data = await client.read(selectedKey, 1);
                 setPassage(data);
+                setSelectedToken(-1);
             } catch (err) {
                 setError(err instanceof Error ? err.message : String(error));
             }
+        })();
+    }, [selectedKey])
+
+    useEffect(() => {
+        if (selectedToken === -1 || !passage) {
+            setSelectedGlosses([]);
+            return;
         }
 
-        load();
-    }, [selectedKey])
+        const updatedGlosses = passage!.glosses.filter((gloss) => gloss.start <= selectedToken && selectedToken <= gloss.end);
+        setSelectedGlosses(updatedGlosses);
+    }, [selectedToken])
 
     if (error) return <pre style={{ color: "red" }}>{error}</pre>;
 
@@ -42,14 +55,25 @@ function App() {
                     value={selectedKey}
                     onChange={(e) => setSelectedKey(e.target.value)}
                 >
-                    {passageOptions.map((passage) => (
-                        <option key={passage.key} value={passage.key}>{passage.name}</option>
+                    {passageOptions.map((passage, i) => (
+                        <option key={i} value={passage.key}>{passage.name}</option>
                     ))}
                 </select>
             </label>
             <div className="py-3 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10" dir="auto">
-                {passage && passage.tokens.map((token) => <TokenBlock key={`${token.content}-${token.offset}`} token={token} />)}
+                {passage && passage.tokens.map(
+                    (token) =>
+                    <TokenBlock
+                        key={`${token.content}-${token.offset}`}
+                        token={token}
+                        setSelectedToken={setSelectedToken}
+                        selected={selectedToken === token.offset}
+                    />
+                )}
             </div>
+            <div>{selectedGlosses.map((gloss, i) => (
+                <div key={i}>{gloss.content}</div>
+            ))}</div>
         </div>
     );
 }
--- src/TokenBlock.tsx
+++ src/TokenBlock.tsx
@@ -1,8 +1,23 @@
+import React from "react";
 import { Token } from "@/types/passage";
 
-export default function TokenBlock({ token }: { token: Token }) {
+export default function TokenBlock(
+    { 
+        token,
+        setSelectedToken,
+        selected,
+    }: { token: Token, setSelectedToken: (offset: number) => void, selected: boolean }) {
     return (
-        <div className="py-1 px-1 hover:bg-sky-100 text-center">
+        <div
+            className={`py-1 px-1 text-center ${selected ? "bg-yellow-200 hover:bg-orange-200": "hover:bg-sky-100"}`}
+            onClick={() => {
+                if (selected) {
+                    setSelectedToken(-1);
+                    return;
+                }
+                setSelectedToken(token.offset);
+            }}
+        >
             <div>{token.content}</div>
             <div>{token.transliteration}</div>
             <div>{token.translation}</div>
